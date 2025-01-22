bb3dc92edcd432aa59b5127da377c23b78b99433
fcoronao <fjavierco@gmail.com>
2025-01-22 12:52:46 -0600

    Add files via upload

---
+++ src/main/java/com/intercam/mx/WsFacturasHonorariosApplication.java
@@ -0,0 +1,12 @@
+package com.intercam.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsFacturasHonorariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsFacturasHonorariosApplication.class, args);
+	}
+}
---
+++ src/main/java/com/intercam/mx/controller/GeneralViewController.java
@@ -0,0 +1,26 @@
+package com.intercam.mx.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author fcoronao
+ *
+ */
+@RestController
+@RequestMapping
+public class GeneralViewController {
+	
+	private static final Logger log = Logger.getLogger(GeneralViewController.class);
+
+	@GetMapping("/hola")
+	public String respuestas() {	
+		
+		String response = "A huevo perro!!";
+		
+		return response;
+	}
+	
+}
